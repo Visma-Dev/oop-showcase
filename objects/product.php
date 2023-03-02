99f16fee442c7a07677907518587ae76c6c4e30a
VISMA-DEV <visma-dev@mail.ru>
2023-03-02 19:12:31 +0300

    created a class for working with products table

---
+++ objects/product.php
@@ -0,0 +1,50 @@
+<?php
+
+class Product
+{
+    // подключение к базе данных и имя таблицы
+    private $conn;
+    private $table_name = "products";
+
+    // свойства объекта
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $category_id;
+    public $timestamp;
+
+    // в конструкторе принимаем значение метода Database->getConnection()
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    // метод создания товара
+    function create()
+    {
+        // Запрос для добавления записей в таблицу products.
+        // Вместо значений подставляем именные плейсхолдеры, тем самым отделяя синтаксис запроса от значений параметров - защищаемся от инъекций.
+        $query = "INSERT INTO " . $this->table_name . " SET name=:name, price=:price, description=:description, category_id=:category_id, created=:created";
+
+        //подготавливаем запрос
+        $stmt = $this->conn->prepare($query);
+
+        // получаем время создания записи
+        $this->timestamp = date("Y-m-d H:i:s");
+
+        // привязываем значения к плейсхолдерам
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->timestamp);
+
+        //выполняем запрос
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
