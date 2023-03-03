5b4302418d748958f66f2f79f7103a7cfdadc8de
VISMA-DEV <visma-dev@mail.ru>
2023-03-04 00:19:51 +0300

    created basic layout files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// установка заголовка страницы
+$page_title = "Витрина со всеми товарами на любой вкус";
+
+require_once "layout/header.php";
+?>
+
+    <div class="right-button-margin">
+        <a href="create_product.php" class="btn btn-default pull-right">Добавить товар</a>
+    </div>
+
+<?php // подвал
+require_once "layout/footer.php";
